d8dcb51b9a45a0c5fbf245ce507496b011f4af40
aherreraorozco <95389185+aherreraorozco@users.noreply.github.com>
2021-12-13 14:33:36 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+
+/**
+ *  @author adileneherreraorozco
+ *  @author jadaross
+ *  
+ *  Final Project
+ *  Dec. 2nd 2021
+*/
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -5,6 +14,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Graph newGraph = new Graph();
 		try {
 			File file = new File("california-data.txt");
 			Scanner input = new Scanner(file);
@@ -14,6 +24,11 @@ public class Main {
 				System.out.println("City A: " + data[0]);
 				System.out.println("City B: " + data[1]);
 				System.out.println("Distance: " + data[2]);
+				newGraph.add(data[0]);
+				newGraph.add(data[1]);
+				double dis = Double.parseDouble(data[2]);
+//				newGraph.addEdge(data[0], data[1], dis);
+				System.out.println("Ran once.");
 			}
 			input.close();
 		} catch (FileNotFoundException e) {
